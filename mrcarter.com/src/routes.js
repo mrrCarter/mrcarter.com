5009525a4cd089f65a1354c5a5a850347ebc5aa8
mrrCarter <carther@mit.edu>
2018-02-14 18:27:25 -0800

    just creating Home.js

---
+++ mrcarter.com/src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
+import Home from "./containers/Home";
+
+export default (
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+        </Route>
+    </Router>
+);
