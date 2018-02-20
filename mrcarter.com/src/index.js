e68da1b01c8aaa2b4d61cfe816913a7c014ced41
mrrCarter <carther@mit.edu>
2018-02-20 13:25:58 -0800

    got cinemagraph picture to render

--- mrcarter.com/src/index.js
+++ mrcarter.com/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './theme/globalstyle';
+
+
+ReactDOM.render(
+    routes, 
+    document.getElementById('root')
+);
+
--- mrcarter.com/src/routes.js
+++ mrcarter.com/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 import Home from "./containers/Home";
+import App from "./containers/App";
 
 export default (
     <Router history={hashHistory}>
